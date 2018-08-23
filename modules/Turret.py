938ff36c3ae63ecc95d928ca80ff052b6c7117af
Dewald928 <26013835@student.g.nwu.ac.za>
2018-08-24 00:27:31 +0200

    servos move with coords pulses

--- modules/Turret.py
+++ modules/Turret.py
@@ -45,19 +45,28 @@ class Controller(threading.Thread):
         self.yMax = float(cfg['controller']['yMax'])
         self.xRatio = (self.camw)/(self.xMax-self.xMin)
         self.yRatio = (self.camh)/(self.yMax-self.yMin)
+        self.xPulse = 0.0
+        self.yPulse = 0.0
 
         print(self.xRatio, self.yRatio)
 
         threading.Thread.__init__(self)
 
     def coordToPulse(self,coord):
-        xPulse = (float(coord[0])/self.xRatio)+self.xMin
-        yPulse = ((self.camh - float(coord[1]))/self.yRatio)+self.yMin
-        print(xPulse,yPulse)
-        return (xPulse,yPulse)
+        self.xPulse = (float(coord[0])/self.xRatio)+self.xMin
+        self.yPulse = ((self.camh - float(coord[1]))/self.yRatio)+self.yMin
+        # print(xPulse,yPulse)
+        return (self.xPulse,self.yPulse)
 
     def fire(self): # pull trigger thread
         print('Skiet skiet')
+
+    def run(self):
+        while True:
+            # print('Turret thread running')
+            sleep(0.01)
+            self.driver.move(self.servoPan, self.xPulse)
+            self.driver.move(self.servoTilt, self.yPulse)
 
 
 
